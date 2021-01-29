ef18f5e53fe371f75f0c97e33565c38f680c9b73
Nicolas BORDES <nicolas.bordes@grenoble-inp.org>
2021-01-29 15:51:28 +0100

    expose prng_fill

--- headers/masked_aes.h
+++ headers/masked_aes.h
@@ -5,6 +5,7 @@ void masked_aes_encrypt128(uint16_t state[8][8], uint16_t rkeys[11][8][8]);
 
 //
 void prng_init(int seed);
+void prng_fill(char *dst, int size);
 void mask_bitslice_state(uint8_t state[16], uint16_t masked_bs_state[8][8]);
 void mask_8(uint16_t v, uint16_t dst[8]);
 void masked_mixcolumns_8(uint16_t state[8][8]);
